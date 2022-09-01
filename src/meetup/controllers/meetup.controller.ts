d2b18aadb3282c4089ab4c0786b7b57a20f8fc57
strawberrystr1 <andrey_overdose@mail.ru>
2022-09-01 11:13:21 +0300

    feat: add validation for dto

--- src/meetup/controllers/meetup.controller.ts
+++ src/meetup/controllers/meetup.controller.ts
@@ -1,68 +1,83 @@
-import meetupService from '../services/user.service'
-import { Request, Response } from 'express'
-import { QueryResult } from 'pg'
+import meetupService from "../services/user.service";
+import { Request, Response } from "express";
+import { QueryResult } from "pg";
 
-import { CreateMeetupDTO } from '../dto/create.meetup.dto'
-import { UpdateMeetupDTO } from '../dto/update.meetup.dto'
-import { GetMeetupDTO } from '../dto/get.meetup.dto'
-import { DeleteMeetupDTO } from '../dto/delete.meetup.dto'
+import { CreateMeetupDTO } from "../dto/create.meetup.dto";
+import { UpdateMeetupDTO } from "../dto/update.meetup.dto";
+import { GetMeetupDTO } from "../dto/get.meetup.dto";
+import { DeleteMeetupDTO } from "../dto/delete.meetup.dto";
+import { createMeetupSchema, updateMeetupSchema } from "../validations/schemas";
+
 
 class MeetupController {
   async getAll(req: Request, res: Response) {
     try {
-      const meetups = await meetupService.getAll()
-      res.json(meetups.rows)
+      const meetups = await meetupService.getAll();
+      res.json(meetups.rows);
     } catch (e) {
-      res.status(500).json({ message: 'unpredictable error' })
+      res.status(500).json({ message: "unpredictable error" });
     }
   }
 
   async getOne(req: Request, res: Response) {
     try {
-      const { id } = req.params
-      const meetup: QueryResult<GetMeetupDTO> = await meetupService.getOne(new GetMeetupDTO(id))
-      res.json(meetup.rows[0])
+      const { id } = req.params;
+      const meetup: QueryResult<GetMeetupDTO> = await meetupService.getOne(new GetMeetupDTO(id));
+      res.json(meetup.rows[0]);
     } catch (e) {
-      res.status(500).json({ message: 'unpredictable error' })
+      res.status(500).json({ message: "unpredictable error" });
     }
   }
 
   async createMeetup(req: Request, res: Response) {
     try {
-      const { title, date, description, place, theme, tags } = req.body
-
-      if ([title, date, description, place, theme, tags].some(e => !e)) {
-        return res.status(400).json({ message: 'incorrect input' })
+      const createMeetupDTO = new CreateMeetupDTO(req.body)
+      const { error } = createMeetupSchema.validate(createMeetupDTO)
+      
+      if (error) {
+        res.status(400).json({ message: error.message })
+        return
       }
 
-      const meetup: QueryResult<CreateMeetupDTO> = await meetupService.createMeetup(new CreateMeetupDTO(req.body))
-      res.json(meetup.rows[0])
+      const meetup: QueryResult<CreateMeetupDTO> =
+        await meetupService.createMeetup(createMeetupDTO);
+      res.json(meetup.rows[0]);
     } catch (e) {
-      res.status(500).json({ message: 'unpredictable error' })
+      res.status(500).json({ message: "unpredictable error" });
     }
   }
 
   async update(req: Request, res: Response) {
     try {
-      const { id } = req.params
+      const { id } = req.params;
 
-      const meetup: QueryResult<UpdateMeetupDTO> = await meetupService.updateMeetup(new UpdateMeetupDTO(id, req.body))
-      
-      res.json(meetup.rows[0])
+      const updateMeetupDTO = new UpdateMeetupDTO(id, req.body)
+      const { error } = updateMeetupSchema.validate(updateMeetupDTO)
+
+      if (error) {
+        res.status(400).json({ message: error.message })
+        return
+      }
+
+      const meetup: QueryResult<UpdateMeetupDTO> =
+        await meetupService.updateMeetup(updateMeetupDTO);
+
+      res.json(meetup.rows[0]);
     } catch (e) {
-      res.status(500).json({ message: 'unpredictable error' })
+      res.status(500).json({ message: "unpredictable error" });
     }
   }
 
   async delete(req: Request, res: Response) {
     try {
-      const { id } = req.params
-      const meetup: QueryResult<DeleteMeetupDTO> = await meetupService.deleteMeetup(new DeleteMeetupDTO(id))
-      res.json(meetup.rows[0])
+      const { id } = req.params;
+      const meetup: QueryResult<DeleteMeetupDTO> =
+        await meetupService.deleteMeetup(new DeleteMeetupDTO(id));
+      res.json(meetup.rows[0]);
     } catch (e) {
-      res.status(500).json({ message: 'unpredictable error' })
+      res.status(500).json({ message: "unpredictable error" });
     }
   }
 }
 
-export default new MeetupController()
+export default new MeetupController();
---
+++ src/meetup/validations/schemas.ts
@@ -0,0 +1,20 @@
+import Joi from "joi";
+
+export const createMeetupSchema = Joi.object({
+  title: Joi.string().required(),
+  date: Joi.date().required(),
+  description: Joi.string().required(),
+  place: Joi.string().required(),
+  theme: Joi.string().required(),
+  tags: Joi.array().items(Joi.string()).required(),
+});
+
+export const updateMeetupSchema = Joi.object({
+  id: Joi.string(),
+  title: Joi.string(),
+  date: Joi.date(),
+  description: Joi.string(),
+  place: Joi.string(),
+  theme: Joi.string(),
+  tags: Joi.array().items(Joi.string()),
+})
